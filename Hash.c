2f2ec3c6ee59132fef5051c5df79a7cd5b307234
FILIPE <92913654+Filipedel@users.noreply.github.com>
2021-10-30 13:54:43 +0200

    Update Hash.c

--- Hash.c
+++ Hash.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Hash.h"
-//not finish yet
+#include<string.h>
+
 
 hash*init(int taille){
+	int i;
 	hash*h=malloc(sizeof(hash*));
 	h->taille=taille;
 	h->tab=(int*)calloc(taille,sizeof(int));
@@ -16,20 +18,44 @@ int fonctionhash(int key,int taille){
 	return l;
 }
 void ajout(int key,hash *h){
-	int taille=(h)->taille;
 	element *e=malloc(sizeof(element*));
 	e->k=key;
-	int key1=fonctionhash(key,taille);
+	int key1=fonctionhash(key,h->taille);
 	if((h)->tab[key1]==0){
 	(h)->tab[key1]=e->k;
 	}
-	else if((h)->tab[key1]!=0){
-		while((h)->tab[key1]!=0&&key1<taille){
-			key1++;
-		}
-	h->tab[key1]=e->k;
+	if((h)->tab[key1]!=0){
+	if(key1>(h->taille/2)){
+		h->taille=ta(h);
+		h->tab=cop(h);
+	}
+	while(h->tab[key1]!=0&&key1<h->taille){
+		key1++;
+	}
+	h->tab[key1]==e->k;
 	}
 }
-
-
-
+int ta(hash*h){
+	return h->taille*2;
+}
+int* cop(hash* h){
+	int j,i,taille=ta(h);
+int *tab=(int*)calloc(taille,sizeof(int*));
+for(j=0;j<taille;j++){
+	tab[j]=h->tab[j];
+}
+return tab;
+}
+void print(hash *h){
+	int i;
+	for(i=0;i<h->taille;i++){
+		printf("[%d]->%d|\n",i,h->tab[i]);
+	}
+}
+void fre(hash *h){
+	int i;
+	for(i=0;h->taille;i++){
+		free(h->tab);
+	}
+	free(h);
+}
