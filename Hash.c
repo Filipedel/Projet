a9719d1b6873fbae4a736cd75524fcc7cf560822
FILIPE <92913654+Filipedel@users.noreply.github.com>
2021-10-30 19:40:40 +0200

    Update Hash.c

--- Hash.c
+++ Hash.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+##include <stdio.h>
 #include <stdlib.h>
 #include "Hash.h"
 #include<string.h>
@@ -6,7 +6,8 @@
 
 hash*init(int taille){
 	int i;
-	hash*h=malloc(sizeof(hash*));
+	hash*h;
+	h=(hash*)calloc(taille,sizeof(hash));
 	h->taille=taille;
 	h->tab=(int*)calloc(taille,sizeof(int));
 	return h;
@@ -21,41 +22,41 @@ void ajout(int key,hash *h){
 	element *e=malloc(sizeof(element*));
 	e->k=key;
 	int key1=fonctionhash(key,h->taille);
-	if((h)->tab[key1]==0){
-	(h)->tab[key1]=e->k;
-	}
-	if((h)->tab[key1]!=0){
 	if(key1>(h->taille/2)){
 		h->taille=ta(h);
 		h->tab=cop(h);
-			while(h->tab[key1]!=0&&key1<h->taille){
+	}
+	if((h)->tab[key1]==0){
+	(h)->tab[key1]=e->k;
+	return;
+}
+	if(h->tab[key1]!=0&&key1<h->taille) {
 		key1++;
 	}
-	h->tab[key1]==e->k;
-	}
-	else{
-	while(h->tab[key1]!=0&&key1<h->taille){
-		key1++;
-	}
-	h->tab[key1]==e->k;
-	}
+	h->tab[key1]=e->k;
+	
 }
-}
+
 int ta(hash*h){
-	return h->taille*5;
+	return h->taille*4;
 }
 int* cop(hash* h){
 	int j,i,taille=ta(h);
 	int *tab;
+	tab=(int*)calloc(taille,sizeof(int));
 	for(j=0;j<taille;j++){
-	if(j==0){
-		for(i=0;i<taille;i++){
-			tab=(int*)calloc(taille,sizeof(int));
-			}
-		}
 	tab[j]=h->tab[j];
 }
 return tab;
+}
+
+void search(hash*h,int key){
+	int i;
+	for(i=0;i<h->taille;i++){
+		if(key==h->tab[i]){
+			printf("La cle existe:%d dans l'indice:[%d]\n",h->tab[i],i);
+		}
+	}
 }
 void print(hash *h){
 	int i;
@@ -70,5 +71,8 @@ void fre(hash *h){
 	}
 	free(h);
 }
+
+
+
 
 
