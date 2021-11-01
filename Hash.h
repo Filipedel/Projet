842e5db7e248a9af6b84e17c16a26637b34c4ab6
FILIPE <92913654+Filipedel@users.noreply.github.com>
2021-11-01 13:31:10 +0100

    Update Hash.h

--- Hash.h
+++ Hash.h
@@ -2,19 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 typedef struct element{
-	int k;
+	char k;
+	int key;
 }element;
 
 typedef struct hash{
-int *tab;
+	char *tab;
 	int taille;
+
 }hash;
 
 hash *init(int taille);
+element *initi(char key,int value);
 int fonctionhash(int key,int taille);
-void ajout(int key,hash *h);
+void ajout(element*e,hash *h);
 int ta(hash*h);
-int * cop(hash*h);
+char * cop(hash*h);
 void print(hash *h);
-void search(hash*h,int key);
+void search(hash*h,element*e);
 void fre(hash*h);
