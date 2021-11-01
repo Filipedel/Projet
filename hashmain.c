63ba85a10187a57dead8145c583a494d67c87712
FILIPE <92913654+Filipedel@users.noreply.github.com>
2021-11-01 13:32:11 +0100

    Update hashmain.c

--- hashmain.c
+++ hashmain.c
@@ -1,21 +1,28 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "Hash.h"
 #include <string.h>
 
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main() {
-	int i;
-hash *h=init(3);
-ajout(2,h);
-ajout(1,h);
-ajout(3,h);
-ajout(9,h);
-ajout(22,h);
-ajout(24,h);
+	element*e=initi('c',10);
+	element*e1=initi('c',10);
+	element *e2=initi('b',8);
+	element *e3=initi('b',21);
+hash *h;
+h=init(3);
+ajout(e,h);
+ajout(e1,h);
+ajout(e2,h);
+ajout(e3,h);
+
 print(h);
+search(h,e);
 fre(h);
-return 0;
+free(e);
+free(e1);
+free(e2);
+return 1;
 }
